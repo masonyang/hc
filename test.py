55c67aa4c2cc0157a4f2a4391e7d5e19f3801875
Hello Mason <yangminsheng@yangminshengs-MacBook-Pro.local>
2018-02-13 23:31:31 +0800

    fix

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-sayNotice = '哈哈'
+sayNotice = '半点报时，现在 23:30'
 
 url = u'http://tts.baidu.com/text2audio?idx=1&tex={0}&cuid=baidu_speech_' \
 u'demo&cod=2&lan=zh&ctp=1&pdt=1&spd=4&per=4&vol=5&pit=5'.format(sayNotice)
