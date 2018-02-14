85b18dd8421d4f6073e3884b81aa8a1351465fd1
Hello Mason <yangminsheng@yangminshengs-MacBook-Pro.local>
2018-02-14 11:52:06 +0800

    fix

--- schedule.py
+++ schedule.py
@@ -96,9 +96,9 @@ class daySchedule(object):
 						if(items['sayNotice']):
 
 							if(items['action']=='timebroadcast'):
-								items['sayNotice'] = items['sayNotice'] + hour_minute_string
+								items['sayNotice'] = items['sayNotice']# + hour_minute_string
 							elif(items['action']=='weatherbroadcast'):
-								items['sayNotice'] = items['sayNotice'] + self.getWeatherInfo()
+								items['sayNotice'] = items['sayNotice']# + self.getWeatherInfo()
 
 							url = u'http://tts.baidu.com/text2audio?idx=1&tex={0}&cuid=baidu_speech_' \
 							u'demo&cod=2&lan=zh&ctp=1&pdt=1&spd=4&per=4&vol=5&pit=5'.format(items['sayNotice'])
