0a3e15ae08190f2ee55b49507f7887dd3b828059
Hello Mason <yangminsheng@yangminshengs-MacBook-Pro.local>
2018-02-14 12:38:48 +0800

    fix

--- weather.py
+++ weather.py
@@ -44,25 +44,25 @@ class weather(object):
 		except IOError:
 			pass
 
-		if(wdata['today_temp_hig']):
-			text='今天{today_weather},最高温度{today_temp_hig}℃,最低温度{today_temp_low}℃. 明天{tomorrow_weather},最高温度{tomorrow_temp_hig}℃,最低温度{tomorrow_temp_low}℃'.format(**wdata)
-		else:
-			text='今天{today_weather},当前气温{current_temp}℃. 明天{tomorrow_weather},最高温度{tomorrow_temp_hig}℃,最低温度{tomorrow_temp_low}℃'.format(**wdata)
-	
-		text = text + '当前室内温度{temp}度，室内湿度{humidity}度.'.format(**hadata)
+		# if(wdata['today_temp_hig']):
+		# 	text='今天{today_weather},最高温度{today_temp_hig}℃,最低温度{today_temp_low}℃. 明天{tomorrow_weather},最高温度{tomorrow_temp_hig}℃,最低温度{tomorrow_temp_low}℃'.format(**wdata)
+		# else:
+		# 	text='今天{today_weather},当前气温{current_temp}℃. 明天{tomorrow_weather},最高温度{tomorrow_temp_hig}℃,最低温度{tomorrow_temp_low}℃'.format(**wdata)
 
-		tomorrow_temp_hig=float(wdata['tomorrow_temp_hig'])
+		text = '当前室内温度{temp}度，室内湿度{humidity}度.'.format(**hadata)
 
-		tomorrow_temp_low=float(wdata['tomorrow_temp_low'])
+		# tomorrow_temp_hig=float(wdata['tomorrow_temp_hig'])
+
+		# tomorrow_temp_low=float(wdata['tomorrow_temp_low'])
 		
 		otherInfo=''
 
-		if(tomorrow_temp_hig>35):
-			otherInfo=" 已经进入高温烘烤模式，注意防暑降温"
-		elif(tomorrow_temp_hig>30):
-			otherInfo="天气开始热起来"
-		elif(tomorrow_temp_low<=0):
-			otherInfo="温度已跌破零点，注意防寒保暖，全副武装"
+		# if(tomorrow_temp_hig>35):
+		# 	otherInfo=" 已经进入高温烘烤模式，注意防暑降温"
+		# elif(tomorrow_temp_hig>30):
+		# 	otherInfo="天气开始热起来"
+		# elif(tomorrow_temp_low<=0):
+		# 	otherInfo="温度已跌破零点，注意防寒保暖，全副武装"
 
 		text = text+otherInfo
 
