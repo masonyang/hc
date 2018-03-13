47a60537097dde52515ea1baecd979f19449d42d
Hello Mason <yangminsheng@yangminshengs-MacBook-Pro.local>
2018-03-13 22:59:37 +0800

    fix

--- voicetools/conversation.py
+++ voicetools/conversation.py
@@ -18,17 +18,17 @@ class conversation(object):
 
 			threshold, transcribed = self.mic.passiveListen(self.persona)
 			
-			if not transcribed or not threshold:
-				print("Nothing has been said or transcribed.")
-				continue
-			else:
-				print("Keyword '%s' has been said!", self.persona)
+			# if not transcribed or not threshold:
+			# 	print("Nothing has been said or transcribed.")
+			# 	continue
+			# else:
+			# 	print("Keyword '%s' has been said!", self.persona)
 
-			input = self.mic.activeListenToAllOptions(threshold)
+			# input = self.mic.activeListenToAllOptions(threshold)
 
-			if input:
-				print(input)
-			else:
-				print("什么?")
+			# if input:
+			# 	print(input)
+			# else:
+			# 	print("什么?")
 
 
