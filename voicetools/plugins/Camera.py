a5d1be5fdb0d5e5e6ac94a02776df26683d5408f
Hello Mason <yangminsheng@yangminshengs-MacBook-Pro.local>
2018-03-14 21:25:57 +0800

    fix

--- voicetools/plugins/Camera.py
+++ voicetools/plugins/Camera.py
@@ -34,68 +34,35 @@ def handle(text, mic, profile):
     sound = True
     usb_camera = True
     # read config
-    if profile[SLUG] and 'enable' in profile[SLUG] and \
-       profile[SLUG]['enable']:
-        if 'count_down' in profile[SLUG] and \
-           profile[SLUG]['count_down'] > 0:
-            count_down = profile[SLUG]['count_down']
-        if 'quality' in profile[SLUG] and \
-           profile[SLUG]['quality'] > 0:
-            quality = profile[SLUG]['quality']
-        if 'dest_path' in profile[SLUG] and \
-           profile[SLUG]['dest_path'] != '':
-            dest_path = profile[SLUG]['dest_path']
-        if 'vertical_flip' in profile[SLUG] and \
-           profile[SLUG]['vertical_flip']:
-            vertical_flip = True
-        if 'horizontal_flip' in profile[SLUG] and \
-           profile[SLUG]['horizontal_flip']:
-            horizontal_flip = True
-        if 'sound' in profile[SLUG] and \
-           not profile[SLUG]['sound']:
-            sound = False
-        if 'usb_camera' in profile[SLUG] and \
-                profile[SLUG]['usb_camera']:
-            usb_camera = True
-        if any(word in text for word in [u"安静", u"偷偷", u"悄悄"]):
-            sound = False
-        try:
-            if not os.path.exists(dest_path):
-                os.makedirs(dest_path)
-        except Exception:
-            mic.say(u"抱歉，照片目录创建失败")
-            return
-        dest_file = os.path.join(dest_path, "%s.jpg" % time.time())
-        if usb_camera:
-            command = "fswebcam --no-banner -r 1024x765 -q "
-            if vertical_flip:
-                command = command+' -s v '
-            if horizontal_flip:
-                command = command+'-s h '
-            command = command+dest_file
-        else:
-            command = ['raspistill', '-o', dest_file, '-q', str(quality)]
-            if count_down > 0 and sound:
-                command.extend(['-t', str(count_down*1000)])
-            if vertical_flip:
-                command.append('-vf')
-            if horizontal_flip:
-                command.append('-hf')
-        if sound and count_down > 0:
-            mic.say(u"收到，%d秒后启动拍照" % (count_down))
-            if usb_camera:
-                time.sleep(count_down)
-
-        process = subprocess.Popen(command, shell=usb_camera)
-        res = process.wait()
-        if res != 0:
-            if sound:
-                mic.say(u"拍照失败，请检查相机是否连接正确")
-            return
-        if sound:
-            mic.play(mic.dingdangpath.data('audio', 'camera.wav'))
+    dest_file = os.path.join(dest_path, "%s.jpg" % time.time())
+    if usb_camera:
+        command = "fswebcam --no-banner -r 1024x765 -q "
+        if vertical_flip:
+            command = command+' -s v '
+        if horizontal_flip:
+            command = command+'-s h '
+        command = command+dest_file
     else:
-        mic.say(u"请先在配置文件中开启相机拍照功能")
+        command = ['raspistill', '-o', dest_file, '-q', str(quality)]
+        if count_down > 0 and sound:
+            command.extend(['-t', str(count_down*1000)])
+        if vertical_flip:
+            command.append('-vf')
+        if horizontal_flip:
+            command.append('-hf')
+    if sound and count_down > 0:
+        mic.say(u"收到，%d秒后启动拍照" % (count_down))
+        if usb_camera:
+            time.sleep(count_down)
+
+    process = subprocess.Popen(command, shell=usb_camera)
+    res = process.wait()
+    if res != 0:
+        if sound:
+            mic.say(u"拍照失败，请检查相机是否连接正确")
+        return
+    if sound:
+        mic.play(mic.dingdangpath.data('audio', 'camera.wav'))
 
 
 def isValid(text):
