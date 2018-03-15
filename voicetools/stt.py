6138cf8bc9e8e3d601e7d24b56426722c29e7301
Hello Mason <yangminsheng@yangminshengs-MacBook-Pro.local>
2018-03-15 22:32:11 +0800

    fix

--- voicetools/stt.py
+++ voicetools/stt.py
@@ -390,7 +390,7 @@ class BaiduSTT(AbstractSTTEngine):
         else:
             transcribed = []
             if text:
-                text.replace('，','')
+                text = text.replace('，','')
                 transcribed.append(text.upper())
 
             # output_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'listen_awakekw.json')
