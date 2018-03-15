58df6ab20993e748210f53d8a0cd72f71903e406
Hello Mason <yangminsheng@yangminshengs-MacBook-Pro.local>
2018-03-15 22:06:21 +0800

    fix

--- voicetools/mic.py
+++ voicetools/mic.py
@@ -248,7 +248,8 @@ class Mic:
         transcribed = self.passive_stt_engine.transcribe_keyword(
             ''.join(frames))
 
-        print(transcribed+'____'+PERSONA)
+        print(transcribed)
+        print(PERSONA)
 
         if transcribed is not None and \
            any(PERSONA in phrase for phrase in transcribed):
